e8fcf4604a36418f78215d4db2e2db90cda79f8f
Ikenna Egbuonu Gary <i.egbuonu@principiumtechnology.com>
2026-05-27 09:11:41 +0100

    Resolved Payout bug

--- src/services/payout.service.ts
+++ src/services/payout.service.ts
@@ -4,12 +4,13 @@ import { QueryPayoutParams } from "@/types";
 
 const PayoutService = {
   getPayoutHistory: (params: QueryPayoutParams) =>
-    api.get("/api/payout/history", { params }).then((r) => r.data),
+    api.get("/api/payout/history", { params }).then((r) => r.data.data),
 
-  getEligibleOrders: () => api.get("/api/payout/eligible").then((r) => r.data),
+  getEligibleOrders: () =>
+    api.get("/api/payout/eligible").then((r) => r.data.data),
 
   requestPayout: (sellerOrderId: string) =>
-    api.post(`/api/payout/request/${sellerOrderId}`).then((r) => r.data),
+    api.post(`/api/payout/request/${sellerOrderId}`).then((r) => r.data.data),
 };
 
 export default PayoutService;
